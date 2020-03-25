4e157189bc4cf54a187b6747030cb0d1df208b6c
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-25 21:35:40 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  newarray = []
+  
+  speakers.each do |name|
+    newarray.push(badge_maker(name))
+  end
+  
+  return newarray
+  
+end
+
+def assign_rooms(speakers)
+  counter = 1 
+  
+  newarray = []
+  
+  speakers.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    
+    counter += 1 
+    
+  end
+  
+  newarray
+  
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |speakers|
+    puts speakers
+  end
+  
+  assign_rooms(speakers).each do |speakers|
+    puts speakers
+  end
+end
