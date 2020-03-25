e1a332d5b0ff2875c98eb82fa2ff23751f37d5f7
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-25 21:23:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,17 @@
 # Write your code here.
 
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  newarray = []
+  
+  speakers.each do |name|
+    newarray.push(badge_maker(name))
+  end
+  
+  return newarray
+  
+end
+
